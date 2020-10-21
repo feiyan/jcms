c91855148e3e1939c6005da392e755cca1e1721d
FeiYan <imfeiyan@gmail.com>
2020-10-21 15:45:50 +0800

    init spring boot

---
+++ src/main/java/com/example/jcms/JcmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcmsApplication.class, args);
+    }
+
+}
